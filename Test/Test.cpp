5073a7fe29873dc77e528dc1566a6e9493af23e9
Manfred Hästmark <hastmark@student.chalmers.se>
2023-02-11 20:45:35 +0100

    Added a visual representation of 1D perlin Noise

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,15 +1,82 @@
 // test.cpp
 
-#include "SDL.h"
 
+#include "Test.h"
+#include "SDL.h"
 #include "Noise.h"
 
-#define WINDOW_WIDTH 600
-#define	WINDOW_HEIGHT 400
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
-	const float frequency = 5;
+	while (1) 
+	{
+		std::cout << "Please enter 1,2 or 3 to showcase the different Perlin noises: ";
+
+		int choice;
+		std::cin >> choice;
+
+		switch (choice)
+		{
+		case Choice::perlin1D:
+			show1dPerlinNoise();
+			break;
+		case Choice::perlin2D:
+			show2dPerlinNoise();
+			break;
+		case Choice::perlin3D:
+			show3dPerlinNoise();
+			break;
+		default:
+			std::cout << "Please enter a valid choice\n";
+			break;
+		}
+	}
+	return 0;
+}
+
+
+/*
+* Graphs a line using 1D perlin noise
+*/
+void show1dPerlinNoise()
+{
+	std::cout << "Please enter frequency: ";
+
+	float frequency;
+	std::cin >> frequency;
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+	float perlin = (Noise::Perlin::calculateNoise(0) + 1) / 2;
+	int y = (int)(perlin * WINDOW_HEIGHT);
+
+	for (int x = 0; x < WINDOW_WIDTH; x++)
+	{
+		float xRel = (x * frequency) / WINDOW_WIDTH;
+		perlin = (Noise::Perlin::calculateNoise(xRel) + 1) / 2;
+		SDL_RenderDrawLine(renderer, x - 1, y, x, (int)(perlin * WINDOW_HEIGHT));
+		y = (int)(perlin * WINDOW_HEIGHT);
+	}
+	
+	SDL_RenderPresent(renderer);
+	SDL_Delay(3000);
+	SDL_DestroyWindow(window);
+}
+
+/*
+* Draws a heightmap lookalike using 2D perlin noise
+*/
+void show2dPerlinNoise()
+{
+	std::cout << "Please enter frequency: ";
+
+	float frequency;
+	std::cin >> frequency;
 
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL_Window* window = SDL_CreateWindow("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
@@ -28,8 +95,15 @@ int main(int argc, char* argv[])
 	}
 
 	SDL_RenderPresent(renderer);
-
 	SDL_Delay(3000);
+	SDL_DestroyWindow(window);
+}
 
-	return 0;
+
+void show3dPerlinNoise()
+{
+	std::cout << "Please enter frequency: ";
+
+	float frequency;
+	std::cin >> frequency;
 }
---
+++ Test/Test.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define WINDOW_WIDTH 600
+#define	WINDOW_HEIGHT 400
+
+
+enum Choice {perlin1D = 1, perlin2D = 2, perlin3D = 3};
+
+void show1dPerlinNoise();
+void show2dPerlinNoise();
+void show3dPerlinNoise();
